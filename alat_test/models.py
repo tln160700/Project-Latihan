ad7ae45eb09e8d22ccf0b7dd9a2011e476f40444
Egi <1811102441070@umkt.ac.id>
2021-10-27 01:11:23 +0800

    commit kedua

--- alat_test/models.py
+++ alat_test/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.fields.files import ImageField
 
 # Create your models here.
+class SoalCPM (models.Model):
+    label = models.CharField(max_length=8, default="")
+    
+
+class JawabanCPM (models.Model):
+    label = models.CharField(max_length=8, default="")
+
