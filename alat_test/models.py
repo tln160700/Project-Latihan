848b4de831c3a24d83e6b628d67336e0e0051c87
Egi <1811102441070@umkt.ac.id>
2021-10-27 16:35:18 +0800

    commit kedua

--- alat_test/models.py
+++ alat_test/models.py
@@ -3,9 +3,10 @@ from django.db.models.fields.files import ImageField
 
 # Create your models here.
 class SoalCPM (models.Model):
-    label = models.CharField(max_length=8, default="")
-    
+    KalimatSoal = models.CharField(max_length=8, default="")
+    GambarSoal = models.ImageField(upload_to = 'CPM/Soal')
 
 class JawabanCPM (models.Model):
-    label = models.CharField(max_length=8, default="")
-
+    KalimatJawaban = models.CharField(max_length=8, default="")
+    GambarJawaban = models.ImageField(upload_to = 'CPM/Jawaban')
+    KonektorJawaban = models.ForeignKey(SoalCPM, on_delete=models.CASCADE)
